94c5b9ca0415c1c73cdf6396f2514be8e8511a88
Lulimartinu <lulimartinucci@hotmail.com>
2023-12-01 00:02:25 -0300

    dark mode

--- fe3-final/src/App.jsx
+++ fe3-final/src/App.jsx
@@ -2,11 +2,18 @@
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import { Outlet } from "react-router-dom";
+
 import Card from "./Components/Card";
 
+
 function App() {
+
+
+
   return (
+
       <div className="App">
+          
           <Navbar/>
           <Card/>
           <Footer/>
--- fe3-final/src/Components/Navbar.jsx
+++ fe3-final/src/Components/Navbar.jsx
@@ -1,32 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
 
   return (
-    <nav >
+    <nav className="dark">
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
+      <li>
+        <Link to="/"> Home </Link>
+      </li>
 
-      
-        <li > 
-          <Link to='/'> Home </Link>
-        </li>
+      <li>
+        <Link to="/contacto"> Contacto </Link>
+      </li>
 
-        <li> 
-          <Link to='/contacto'> Contacto </Link>
-        </li>
+      <li>
+        <Link to="/favs"> Favs </Link>
+      </li>
 
-        <li > 
-          <Link to='/favs'> Favs  </Link>
-        </li>
-
-
-      
-      <button>Change theme</button>
+      <button >Change theme</button>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
